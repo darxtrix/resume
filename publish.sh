5a133fac3fd930b0ea4a81160f3404d4f3643e2a
Ankush Sharma <ankprashar@gmail.com>
2017-02-20 00:53:14 +0530

    Updating the resume template

--- publish.sh
+++ publish.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # Generate new resume
 echo "\033[0;32m Jekyll building....\n \033[0m"
 
@@ -18,7 +16,7 @@ git commit -m "Updating the resume template"
 
 if [ $? -eq 0 ]; then
     git push origin master
-    git push origin gh-pages -f
+    git push origin master:gh-pages -f
 else
     echo -e "\033[1;31m No changes made to the resume \033[0m"
 fi
