f443d31a722d8d21b15586bdfa62d593bb4b7c9c
Ankush Sharma <ankprashar@gmail.com>
2017-02-20 00:04:30 +0530

    Updating the resume template

--- publish.sh
+++ publish.sh
@@ -13,12 +13,12 @@ echo "\x1B[0;32m Updating resume on gh-pages ..\n \x1b[m"
 
 git add index.html media/*
 
-git commit -m "Updating the resume :wink:"
+git commit -m "Updating the resume :wink:" 
 
 if [$? -eq 0]; then
     git push origin gh-pages
 else
-    echo "No changes made to the resume."
+    echo -e "\033[1;31m No changes made to the resume \033[0m"
 fi
 
 # Update master
@@ -31,7 +31,7 @@ git commit -m "Updating the resume template"
 if [$? -eq 0]; then
     git push origin master
 else
-    echo "No changes made to the resume template."
+    echo -e "\033[1;31m No changes made to the resume template \033[0m"
 fi
 
 
