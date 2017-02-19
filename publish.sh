fec48d88c2c7721a955b4a58d7cc38b287d340f2
Ankush Sharma <ankprashar@gmail.com>
2017-02-20 00:07:25 +0530

    Updating the resume template

--- publish.sh
+++ publish.sh
@@ -2,36 +2,39 @@
 rm ./index.html
 
 # Generate new resume
-echo "\x1B[0;32m Jekyll building....\n \x1b[m"
+echo "\033[0;32m Jekyll building....\n \x1b[m"
 
 jekyll build
 
 cp _site/index.html ./index.html
 
+echo "\n\n"
+
 # Update gh-pages
-echo "\x1B[0;32m Updating resume on gh-pages ..\n \x1b[m"
+echo "\033[0;32m Updating resume on gh-pages ..\n \x1b[m"
 
 git add index.html media/*
 
-git commit -m "Updating the resume :wink:" 
+git commit -m "Updating the resume :wink:"
 
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
     git push origin gh-pages
 else
     echo -e "\033[1;31m No changes made to the resume \033[0m"
 fi
 
+echo "\n\n"
+
+
 # Update master
-echo "\x1B[0;32m Updating resume template on master ..\n \x1b[m"
+echo "\033[0;32m Updating resume template on master ..\n \x1b[m"
 
 git add index.md publish.sh README.md
 
 git commit -m "Updating the resume template"
 
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
     git push origin master
 else
     echo -e "\033[1;31m No changes made to the resume template \033[0m"
 fi
-
-
